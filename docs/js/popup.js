0645d12c51cbbbfc86ff0914564714deaa08dada
kotik-tortik <ni899707@gmail.com>
2020-07-17 16:29:04 +0600

    files in docs folder were updated

--- docs/js/popup.js
+++ docs/js/popup.js
@@ -1,29 +1,31 @@
 'use strict'
 
-const popupOpenBtn = document.querySelector('.popup-btn');
-const popup = document.querySelector('.popup');
-const popupOverlay = document.querySelector('.popup-overlay');
-const popupSubmitBtn = popup.querySelector('.popup__submit-btn');
-const ESC_BUTTON_KEY_CODE = 27;
-
-popupOpenBtn.onclick = () => {
-  popupOverlay.classList.add('open-popup');
-  popup.classList.add('open-popup');
-}
-
-window.addEventListener('keydown', (e) => {
-  if (e.keyCode === ESC_BUTTON_KEY_CODE) {
-    e.preventDefault();
+if (document.querySelector('.popup')) {
+  const popupOpenBtn = document.querySelector('.popup-btn');
+  const popup = document.querySelector('.popup');
+  const popupOverlay = document.querySelector('.popup-overlay');
+  const popupSubmitBtn = popup.querySelector('.popup__submit-btn');
+  const ESC_BUTTON_KEY_CODE = 27;
+  
+  popupOpenBtn.onclick = () => {
+    popupOverlay.classList.add('open-popup');
+    popup.classList.add('open-popup');
+  }
+  
+  window.addEventListener('keydown', (e) => {
+    if (e.keyCode === ESC_BUTTON_KEY_CODE) {
+      e.preventDefault();
+      popup.classList.remove('open-popup');
+      popupOverlay.classList.remove('open-popup');
+    }
+  });
+  
+  popupOverlay.onclick = () => {
     popup.classList.remove('open-popup');
     popupOverlay.classList.remove('open-popup');
   }
-});
-
-popupOverlay.onclick = () => {
-  popup.classList.remove('open-popup');
-  popupOverlay.classList.remove('open-popup');
-}
-
-popupSubmitBtn.onclick = (e) => {
-  e.preventDefault();
+  
+  popupSubmitBtn.onclick = (e) => {
+    e.preventDefault();
+  }
 }
